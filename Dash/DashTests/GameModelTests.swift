e223068a27f41f15e3383f65efeae29ffdb92c01
jieliangang <jieliangang@gmail.com>
2019-04-07 02:31:00 +0800

    Add unit test stubs

---
+++ Dash/DashTests/GameModelTests.swift
@@ -0,0 +1,13 @@
+//
+//  GameModelTests.swift
+//  DashTests
+//
+//  Created by Jie Liang Ang on 7/4/19.
+//  Copyright © 2019 nus.cs3217. All rights reserved.
+//
+
+import XCTest
+
+class GameModelTests: XCTestCase {
+
+}
