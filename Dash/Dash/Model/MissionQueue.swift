737fb66dea3d883c1d15247503f2e5e73da123d9
jo-lyn <e0007247@u.nus.edu>
2019-04-11 12:28:35 +0800

    Add mission queue

---
+++ Dash/Dash/Model/MissionQueue.swift
@@ -0,0 +1,23 @@
+//
+//  MissionQueue.swift
+//  Dash
+//
+//  Created by Jolyn Tan on 11/4/19.
+//  Copyright © 2019 nus.cs3217. All rights reserved.
+//
+
+import Foundation
+
+struct MissionQueue {
+    var distance = Queue<Double>([
+        1000,
+        2000,
+        3000,
+        4000,
+        5000,
+        6000,
+        7000,
+        10000,
+        15000
+    ])
+}
