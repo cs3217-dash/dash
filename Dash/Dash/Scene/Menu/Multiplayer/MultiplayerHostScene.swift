3850f55eac4269f565459a68347ee8dee253e3b3
jo-lyn <e0007247@u.nus.edu>
2019-04-16 16:28:07 +0800

    Add multiplayer lobby scene

---
+++ Dash/Dash/Scene/Menu/Multiplayer/MultiplayerHostScene.swift
@@ -0,0 +1,35 @@
+//
+//  MultiplayerHostScene.swift
+//  Dash
+//
+//  Created by Jolyn Tan on 16/4/19.
+//  Copyright © 2019 nus.cs3217. All rights reserved.
+//
+
+import SpriteKit
+
+class MultiplayerHostScene: SKScene {
+
+    override func didMove(to view: SKView) {
+        initRoomIdLabel()
+        initPlayerCountLabel()
+        initModeSelectionLabel()
+        initStartLabel()
+    }
+
+    private func initRoomIdLabel() {
+
+    }
+
+    private func initPlayerCountLabel() {
+
+    }
+
+    private func initModeSelectionLabel() {
+
+    }
+
+    private func initStartLabel() {
+
+    }
+}
